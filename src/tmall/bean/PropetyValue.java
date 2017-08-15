906b513bcf053d63685e745ef436c149a3c3e7e1
TurboLoong <2312987772@qq.com>
2017-08-15 23:51:03 +0800

    1建立DAO

--- src/tmall/bean/PropetyValue.java
+++ src/tmall/bean/PropetyValue.java
@@ -4,4 +4,31 @@ package tmall.bean;
  * Created by TurboLoong on 2017/8/13.
  */
 public class PropetyValue {
+    private String value;
+    private Product product;
+    private Product property;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Product getProperty() {
+        return property;
+    }
+
+    public void setProperty(Product property) {
+        this.property = property;
+    }
 }
--- src/tmall/bean/Review.java
+++ src/tmall/bean/Review.java
@@ -1,7 +1,53 @@
 package tmall.bean;
 
+import java.util.Date;
 /**
  * Created by TurboLoong on 2017/8/13.
  */
 public class Review {
+    private String content;
+    private Date createDate;
+    private User user;
+    private Product Product;
+    private int id;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public tmall.bean.Product getProduct() {
+        return Product;
+    }
+
+    public void setProduct(tmall.bean.Product product) {
+        Product = product;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
